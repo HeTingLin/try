1edaf9962cc731e746f64a5f553dce4e49826261
HeTingLin <52898999+HeTingLin@users.noreply.github.com>
2019-07-15 11:40:04 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,StickerSendMessage
+    MessageEvent, TextMessage, TextSendMessage,StickerSendMessage,ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -42,8 +42,13 @@ def handle_message(event):
     msg = event.message.text
     if "貼圖" in msg or "sticker" in msg:
         message=StickerSendMessage(
-            package_id='1',
-            sticker_id='1')
+            package_id=random.randint(0, len(package_ids) - 1),
+            sticker_id=random.randint(0, len(sticker_ids) - 1))
+        line_bot_api.reply_message(event.reply_token,message)
+    if "圖片" in msg or "picture" in msg:
+        message=ImageSendMessage(
+           original_content_url='https://www.google.com/search?biw=1280&bih=529&tbm=isch&sa=1&ei=tfQrXZKbEpuGoATh7LbACg&q=%E5%A4%AA%E9%99%BD&oq=%E5%A4%AA&gs_l=img.1.0.0l10.8164.12558..14308...0.0..1.906.1170.4j6-1......0....1..gws-wiz-img.....0.r3S2wCE8cAY#imgrc=Vj-PdRiIWZyZXM:',
+           preview_image_url='https://www.google.com/search?biw=1280&bih=529&tbm=isch&sa=1&ei=tfQrXZKbEpuGoATh7LbACg&q=%E5%A4%AA%E9%99%BD&oq=%E5%A4%AA&gs_l=img.1.0.0l10.8164.12558..14308...0.0..1.906.1170.4j6-1......0....1..gws-wiz-img.....0.r3S2wCE8cAY#imgrc=Vj-PdRiIWZyZXM:')
         line_bot_api.reply_message(event.reply_token,message)
     # 回應使用者輸入的話
     #line_bot_api.reply_message(
