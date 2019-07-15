16ee40a5c71968f3f3aa623bfa6465c25a129c0a
HeTingLin <52898999+HeTingLin@users.noreply.github.com>
2019-07-15 11:13:47 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -40,9 +40,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 回應使用者輸入的話
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    #line_bot_api.reply_message(
+        #event.reply_token,
+        #TextSendMessage(text=event.message.text))
+      message=StickerSendMessage(
+          package_id='1',
+          sticker_id='1'
+      )
+    line_bot_api.reply_message(event.reply_token,message)
 
 
 if __name__ == "__main__":
